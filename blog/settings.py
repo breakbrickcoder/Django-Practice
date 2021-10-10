778d022489574cda950c81f45740e40596428ece
breakbrickcoder <davidgurung826@gmail.com>
2021-10-10 17:20:37 +0545

    migration to progresql

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home','travello',
+     'travello',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD':'binod',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
