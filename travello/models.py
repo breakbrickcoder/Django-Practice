3318cbbcce3df067632a396adc4923f07ef9df77
breakbrickcoder <davidgurung826@gmail.com>
2021-10-10 17:54:05 +0545

    admin

--- travello/models.py
+++ travello/models.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
 	name = models.CharField(max_length = 100)
 	img = models.ImageField(upload_to = 'pics')
 	desc = models.TextField()
-	price = models.IntegerField()
+	price = models.IntegerField(default = 0)
 	offer = models.BooleanField(default=False)
