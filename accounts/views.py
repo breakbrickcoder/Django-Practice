686e9f765e785c9e2185b40e10056629732f65b4
breakbrickcoder <davidgurung826@gmail.com>
2021-10-12 08:47:21 +0545

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
 # Create your views here.
 def register(request):
 
@@ -13,16 +14,18 @@ def register(request):
 		
 		if password1==password2:
 			if User.objects.filter(username=username).exists():
-				print('Username taken')
+				messages.info(request,'Username is already taken')
+				return redirect('register')
 			elif User.objects.filter(email=email).exists():
-				print('email taken')
+				messages.info(request,'Email is already taken')
+				return redirect('register')
 			else:
 				user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
 				user.save()
-				print('user created')
+				messages.info(request,'user created')
 
 		else:
-			print('password not matching..')
-			return redirect('/')
-	else:
-		return render(request,'register.html')
+			messages.info(request,'password not matching..')
+		return redirect('register')
+	
+	return render(request,'register.html')
